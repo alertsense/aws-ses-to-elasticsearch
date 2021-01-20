863ac637833f58f2f2b0c2ce78e3ce1cd93fdd23
Josiah Peters <josiah.peters@alertsense.com>
2021-01-20 14:05:28 -0700

    Added support for signing requests to AWS ElasticSearch and Specifying Index Patterns

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
-const request = require('superagent');
-const baseUrl = process.env.BASE_URL;
-const user = process.env.USER;
-const pass = process.env.PASSWORD;
+const esName = process.env.ENDPOINT;
 const prefix = process.env.PREFIX;
+const region = process.env.REGION;
 
-exports.handler = function(event, context, callback) {
+var AWS = require('aws-sdk');
+var path = require('path');
+
+/* == Globals == */
+var esDomain = {
+  region: region,
+  endpoint: esName,
+  index: `${prefix}-${getDate()}`,
+  doctype: 'emails'
+};
+var endpoint = new AWS.Endpoint(esDomain.endpoint);
+var creds = new AWS.EnvironmentCredentials('AWS');
+
+
+exports.handler = function (event, context, callback) {
+
   const payload = event.Records[0].Sns;
   payload.Message = JSON.parse(payload.Message);
-  const index = `${prefix}-${getDate()}`;
-  const type = 'emails';
   const id = payload.MessageId;
+  const requestPath = path.join('/', esDomain.index, esDomain.doctype, id, '_create');
 
-  request
-    .put(`${baseUrl}/${index}/${type}/${id}/_create`)
-    .auth(user, pass)
-    .send({ data: payload })
-    .end(err => {
-      if (err) {
-        return callback(err);
-      }
+  var req = new AWS.HttpRequest(endpoint);
+
+  console.log('sending request to:', requestPath);
+
+  const docRequest = JSON.stringify(payload);
+
+  req.method = 'POST';
+  req.path = requestPath;
+  req.region = esDomain.region;
+  req.headers['presigned-expires'] = false;
+  req.headers['Host'] = endpoint.host;
+  req.body = docRequest;
+
+  var signer = new AWS.Signers.V4(req, 'es');  // es: service code
+  signer.addAuthorization(creds, new Date());
+
+  var send = new AWS.NodeHttpClient();
+  send.handleRequest(req, null, function (httpResp) {
+    var respBody = '';
+    httpResp.on('data', function (chunk) {
+      respBody += chunk;
     });
+    httpResp.on('end', function (chunk) {
+      console.log('Response: ' + respBody);
+      context.succeed(`Published SNS Message to ElasticSearch ${id}`);
+    });
+  }, function (err) {
+    console.log('Error: ' + err);
+    context.fail('Lambda failed with error ' + err);
+  });
 };
-
-// return the date of first day of the current month
-// for example '2017-12-06'
 function getDate() {
   const now = new Date();
   return new Date(now.getFullYear(), now.getMonth(), 1).toISOString().substring(0, 10);
